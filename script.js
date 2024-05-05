d855bd6be83a5948b83354e7696993a59512238e
codeNameTwilight <168977315+codeNameTwilight@users.noreply.github.com>
2024-05-05 19:53:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function show() {
+    document.getElementById('cat1').style.visibility = 'visible';
+    document.getElementById('cat2').style.visibility = 'visible';
+    document.getElementById('head').style.visibility = "visible";
+    document.getElementById('mainimg').style.visibility = 'visible';
+    document.getElementById('hbd').style.visibility = 'visible';
+    document.getElementById('name').style.visibility = 'visible';
+    document.querySelector('.wishers').style.visibility = 'visible';
+    document.querySelector('.content').style.visibility = 'visible';
+    document.getElementById('audio').play();
+    document.getElementById('button').style.visibility = 'hidden';
+}
